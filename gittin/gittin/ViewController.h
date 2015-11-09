be4664b4644029790cc7980a0ccc7874dcfe594a
zhouzhaohong <543156293@qq.com>
2015-11-09 10:07:14 +0800

    搭建框架

---
+++ gittin/gittin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittin
+//
+//  Created by 周钊鸿 on 15/11/9.
+//  Copyright © 2015年 小胖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gittin/gittin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gittin
+//
+//  Created by 周钊鸿 on 15/11/9.
+//  Copyright © 2015年 小胖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
